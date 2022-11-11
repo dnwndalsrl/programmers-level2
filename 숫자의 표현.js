721273a9e13f58d5da5a566b96a0944ac5fb473e
dnwndalsrl <dnwndwlsqja@gmail.com>
2022-11-11 13:59:01 +0900

    add

--- 숫자의 표현.js
+++ 숫자의 표현.js
@@ -17,3 +17,18 @@ function solution(n) {
     }
     return answer;
 }
+
+function solution(n) {
+    let answer = 1;
+    for (let i = 1; i < n; i++) {
+        let tmp = 0;
+        for (let j = i; j < n; j++) {
+            tmp += j;
+            if (tmp >= n) {
+                if (tmp === n) answer++;
+                break;
+            }
+        }
+    }
+    return answer;
+  }
